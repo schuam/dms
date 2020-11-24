ecad77f9a5d2f63af7e36dca5a49e1d80e537c42
Andreas Schuster <git@schuam.de>
2020-11-24 21:37:46 +0100

    Move generic parts into separate script
    This was done to be able to reuse it for other dmenu scripts as well.

--- dmenu_scrot.py
+++ dmenu_scrot.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
-from subprocess import run
 from os.path import basename
 import os
+from dmenu_wrapper import run_dmenu
 
 
 NOTIFICATION_TIME = 3000    # ms
@@ -26,24 +26,5 @@ choices = {
             COMMAND_TEMPLATE.format("-s ", SECTION_NOTIFICATION),
 }
 
-d_choises = "\n".join(choices.keys())
-d_cmd = 'echo -e "{}" | dmenu -i -l {:d} -p "{}"'.format(
-        d_choises, len(choices), question
-)
-
-result = run(d_cmd, shell=True, capture_output=True)
-if result.returncode == 0:
-    chosen = result.stdout.decode("utf-8").rstrip()
-
-    if chosen in choices.keys():
-        result = run(choices[chosen], shell=True)
-    else:
-        notify_cmd = "notify-send -u low -t {:d} '{}' " \
-                     "'You made an invalid choice!'".format(
-                     NOTIFICATION_TIME, basename(__file__))
-        run(notify_cmd, shell=True)
-else:
-    # The user quite dmenu by pressing <esc>, don't do anyhting, probably no
-    # action is wanted. Especially don't send an annoying notification.
-    pass
+run_dmenu(question, choices, basename(__file__))
 
---
+++ dmenu_wrapper.py
@@ -0,0 +1,33 @@
+from subprocess import run
+from os.path import basename
+
+
+NOTIFICATION_TIME = 3000    # ms
+
+def run_dmenu(question, choices, script_name=None):
+    dmenu_choices = "\n".join(choices.keys())
+    dmenu_cmd = 'echo -e "{}" | dmenu -i -l {:d} -p "{}"'.format(
+            dmenu_choices, len(choices), question
+    )
+
+    result = run(dmenu_cmd, shell=True, capture_output=True)
+    if result.returncode == 0:
+        chosen = result.stdout.decode("utf-8").rstrip()
+
+        if chosen in choices.keys():
+            result = run(choices[chosen], shell=True)
+        else:
+            script = script_name
+            if script == None:
+                script = basename(__file__)
+
+            notify_cmd = "notify-send -u low -t {:d} '{}' " \
+                        "'You made an invalid choice!'".format(
+                            NOTIFICATION_TIME, script
+            )
+            run(notify_cmd, shell=True)
+    else:
+        # The user quite dmenu by pressing <esc>, don't do anyhting, probably
+        # no action is wanted. Especially don't send an annoying notification.
+        pass
+

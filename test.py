491f604f0e55f79a5d5e3c657f3ccca0228eb6f9
Andreas Schuster <git@schuam.de>
2020-11-24 19:26:12 +0100

    Convert script into my scrot script
    I already have a script that uses scrot to take screenshots. Now this
    script does the same thing.
    
    Next step will be to clean up the code a little bit. And afterwards move
    some parts into an extra script, that can be used for other dmenu
    scripts. The whole execution of dmenu should go into that extra script.

--- test.py
+++ test.py
@@ -2,14 +2,28 @@
 
 from subprocess import run
 from os.path import basename
+import os
 
 
 NOTIFICATION_TIME = 3000    # ms
+IMG_DIR = "$XDG_PICTURES_DIR/screenshots"
+IMG_NAME = "scrot_%Y-%m-%d_%H-%M-%S.png"
+IMG_PATH = os.path.join(IMG_DIR, IMG_NAME)
+FULLSCREEN_NOTIFICATION = "Fullscreen taken and saved"
+SECTION_NOTIFICATION = "Scetion screenshot taken and saved"
+COMMAND_TEMPLATE = "scrot -z {} {} && " \
+                   "notify-send -u low -t {} 'Scrot' '{}'".format(
+                        "{}", IMG_PATH, NOTIFICATION_TIME, "{}"
+)
 
-question = "What's up dude? "
+question = "Screenshot will be saved in {}: ".format(IMG_DIR)
 choices = {
-    "test a": "mkdir a; mkdir z",
-    "test b": "mkdir b; mkdir y",
+    "1. quick fullscreen": \
+            COMMAND_TEMPLATE.format("-d 1", FULLSCREEN_NOTIFICATION),
+    "2. delayed fullscreen": \
+            COMMAND_TEMPLATE.format("-d 4", FULLSCREEN_NOTIFICATION),
+    "3. selection": \
+            COMMAND_TEMPLATE.format("-s ", SECTION_NOTIFICATION),
 }
 
 d_choises = "\n".join(choices.keys())

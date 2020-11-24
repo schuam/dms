464d3bbc027bf4bb876d369689974d95144c8fb1
Andreas Schuster <git@schuam.de>
2020-11-24 18:10:05 +0100

    Add some error handling
    In case the user quits dmenu by pressing <esc> the return code will be
    non zero. This case is handled now. Also if the user types something
    into the dmenu field, that is not a valid choise, that case is handled
    now.
    
    Well handled is not quite correct. For now an error message is printed.

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
-from subprocess import check_output
-from subprocess import call
+from subprocess import run
 
 question = "What's up dude? "
 choices = {
@@ -14,6 +13,17 @@ d_cmd = 'echo -e "{}" | dmenu -i -l {:d} -p "{}"'.format(
         d_choises, len(choices), question
 )
 
-chosen = check_output(d_cmd, shell=True).decode("utf-8").rstrip()
-call(choices[chosen], shell=True)
+result = run(d_cmd, shell=True, capture_output=True)
+if result.returncode == 0:
+    chosen = result.stdout.decode("utf-8").rstrip()
+
+    if chosen in choices.keys():
+        print("Good choice: Let's do this!")
+        result = run(choices[chosen], shell=True)
+    else:
+        # TODO: Notify user of invalid choice. Get rid of print()
+        print("Invalid choice")
+else:
+    # TODO: Notify user of dmenu error code. Get rid of print()
+    print("Something when wrong")
 

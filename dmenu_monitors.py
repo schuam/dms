36a062035a8f546e1d0df250a28f4f55fa437bde
Andreas Schuster <git@schuam.de>
2020-11-24 21:48:53 +0100

    Add python version of my monitor settings script

---
+++ dmenu_monitors.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+from os.path import basename
+import os
+from dmenu_wrapper import run_dmenu
+
+
+LAYOUT_SCRIPTS_DIR = "~/.dotfiles/.screenlayout"
+
+layout_script_paths = [
+    os.path.join(LAYOUT_SCRIPTS_DIR, "docked.sh"),
+    os.path.join(LAYOUT_SCRIPTS_DIR, "mobile.sh"),
+    os.path.join(LAYOUT_SCRIPTS_DIR, "one_external.sh"),
+]
+
+question = "Change monitor settiong to: "
+choices = {
+    "1. docked": layout_script_paths[0],
+    "2. mobile": layout_script_paths[1],
+    "3. one external": layout_script_paths[2],
+}
+
+run_dmenu(question, choices, basename(__file__))
+

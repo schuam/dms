d30dcc635c0f221827ebbc83d0896f52e732ca2b
Andreas Schuster <git@schuam.de>
2020-11-25 20:20:07 +0100

    Add a return value
    In order to be able to determine in the calling script whether or not
    dmenu run successfully and with a valid choise, I added the return
    value. This might be usefull if in the calling script you want to do
    something more, in case the dmenu wrapper was successful (or not).

--- dmenu_wrapper.py
+++ dmenu_wrapper.py
@@ -10,11 +10,13 @@ def run_dmenu(question, choices, script_name=None):
             dmenu_choices, len(choices), question
     )
 
+    success = False
     result = run(dmenu_cmd, shell=True, capture_output=True)
     if result.returncode == 0:
         chosen = result.stdout.decode("utf-8").rstrip()
 
         if chosen in choices.keys():
+            success = True
             result = run(choices[chosen], shell=True)
         else:
             script = script_name
@@ -30,4 +32,6 @@ def run_dmenu(question, choices, script_name=None):
         # The user quite dmenu by pressing <esc>, don't do anyhting, probably
         # no action is wanted. Especially don't send an annoying notification.
         pass
+
+    return success
 

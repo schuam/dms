2cfbf7e4b8247276e5e0b4cd29c23ca130e2522b
Andreas Schuster <git@schuam.de>
2020-12-29 14:20:11 +0100

    Add new screenlayout
    Laptop monitor plus a mirrored external.

--- src/dmenu_monitors.py
+++ src/dmenu_monitors.py
@@ -20,6 +20,7 @@ LAYOUT_SCRIPT_PATHS = [
     os.path.join(LAYOUT_SCRIPTS_DIR, "laptop.sh"),
     os.path.join(LAYOUT_SCRIPTS_DIR, "laptop_plus_one_external.sh"),
     os.path.join(LAYOUT_SCRIPTS_DIR, "laptop_plus_two_external.sh"),
+    os.path.join(LAYOUT_SCRIPTS_DIR, "laptop_plus_mirror.sh"),
     os.path.join(LAYOUT_SCRIPTS_DIR, "one_external.sh"),
     os.path.join(LAYOUT_SCRIPTS_DIR, "two_external.sh"),
 ]
@@ -34,8 +35,9 @@ choices = {
     "1. laptop": LAYOUT_SCRIPT_PATHS[0],
     "2. laptop plus one external": LAYOUT_SCRIPT_PATHS[1],
     "3. laptop plus two external": LAYOUT_SCRIPT_PATHS[2],
-    "5. one external": LAYOUT_SCRIPT_PATHS[3],
-    "6. two external": LAYOUT_SCRIPT_PATHS[4],
+    "4. laptop plus mirror": LAYOUT_SCRIPT_PATHS[3],
+    "5. one external": LAYOUT_SCRIPT_PATHS[4],
+    "6. two external": LAYOUT_SCRIPT_PATHS[5],
 }
 
 
